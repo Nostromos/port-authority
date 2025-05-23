5f3067379f3215c4c1f1b76fdba7d9f8c7820cde
Michael Monaghan <michael@monaghan.nyc>
2025-05-23 13:41:54 -0400

    Write 200ok status to connection

--- app/main.go
+++ app/main.go
@@ -22,9 +22,22 @@ func main() {
 		os.Exit(1)
 	}
 	
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("ERROR: Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	// conn.Write() returns n, err where n is the number of bytes from your byteslice that weresuccessfully written to the connection
+	status := "HTTP/1.1 200 OK\r\n\r\n"
+	total := 0
+
+	for total < len(status) {
+		n, err := conn.Write([]byte(status))
+		total += n
+		if err != nil {
+			fmt.Println("ERROR: timed out or some other problem")
+			os.Exit(1)
+		}
 	}
 }
