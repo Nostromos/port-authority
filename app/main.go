1c3e053f5925e1c933e40cc1c0d8234bca33123f
Michael Monaghan <michael@monaghan.nyc>
2025-05-29 14:50:58 -0400

    Structure codebase to encourage refactor

--- app/main.go
+++ app/main.go
@@ -13,24 +13,28 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("ERROR: Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
-	// Extract URL path from HTTP request
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("ERROR: Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+
+}
+
+
+	/* On connection, createa  buffer and read into it */
 	buf := make([]byte, 4096)
 	n, err := conn.Read(buf)
 	// write loop that populates and ensures all data gets written into buffer
@@ -38,7 +42,7 @@ func main() {
 		fmt.Println("ERROR: Something bad happened while readin the connection")
 		os.Exit(1)
 	}
-
+``
 	// conn.Write() returns n, err where n is the number of bytes from your byteslice that weresuccessfully written to the connection
 	okStatus := "HTTP/1.1 200 OK\r\n\r\n"
 	notFoundStatus := "HTTP/1.1 404 Not Found\r\n\r\n"
@@ -61,8 +65,6 @@ func main() {
 		// total += n
 		os.Exit(0)
 	}
-}
-
 // listening accepting writing in main
 
 // short circuiting / request validation
@@ -108,11 +110,11 @@ type ResponseOptions struct {
 }
 
 type HTTPResponse struct {
-
+	ResponseOptions
 }
 
 type HTTPHeader struct {
-	
+
 }
 
 // Takes an options struct
@@ -123,7 +125,7 @@ func buildResponse(opts ResponseOptions) *HTTPResponse {
 	}
 
 	resp := &HTTPResponse{
-		StatusCode: status,
+		StadtusCode: status,
 		Header: make(HTTPHeader),
 		Body: opts.Body,
 	}
